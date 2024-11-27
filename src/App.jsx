e5cd70f53cc7764697728af99ec008caa97ae41f
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-27 14:19:48 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Posts from "./pages/Posts"
 import About from "./pages/About"
 import Layout from "./pages/Layout"
 import PostDetail from "./pages/PostDetail"
+import { PostProvider } from "./context/PostContext"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/posts/:slug" element={<PostDetail />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PostProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/posts" element={<Posts />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/posts/:slug" element={<PostDetail />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PostProvider>
     </>
   )
 }
